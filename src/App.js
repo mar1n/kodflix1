12b4b82b14923cc6eafec36454e5af180e46bdab
mar1n <30738216+mar1n@users.noreply.github.com>
2019-10-29 11:32:11 +0000

    I add covers for movies as img.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import javascriptLogo from './JavaScript-logo.png';
+import football from './football.png';
+import basketball from './basketball.png';
+import tennis from './tennis.jpg';
+import rugby from './rugby.png';
+import pingpong from './ping-pong.jpg';
+import dart from './dart.jpg';
 import './App.css';
 
 function App() {
@@ -9,14 +15,22 @@ function App() {
       <img src={javascriptLogo} alt='Javascript Logo' />
       
       <div className='containter'>
-        <div className='item'>Football</div>
-        <div className='item'>Baskteball</div>
-        <div className='item'>Rugby</div>
+       
+        <div className='item'>
+        <img src={football} alt='Football' />
+          </div>
+        
+        <div className='item'><img src={basketball} alt='Basketball' /></div>
+        
+        <div className='item'><img src={rugby} alt='Rugby' /></div>
       </div>
       <div className='containter'>
-        <div className='item'>Tennis</div>
-        <div className='item'>Ping pong</div>
-        <div className='item'>Dart</div>
+      
+        <div className='item'><img src={tennis} alt='Tennis' /></div>
+        
+        <div className='item'><img src={pingpong} alt='Ping pong' /></div>
+        
+        <div className='item'><img src={dart} alt='Dart' /></div>
       </div>
     </div>
   );
