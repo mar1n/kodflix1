97c0575f49d21adc260d40b9b7e308b4421fb4d9
mar1n <30738216+mar1n@users.noreply.github.com>
2019-11-12 09:50:09 +0000

    Add new route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Activity from './Activity.js';
 import Details from './Details.js';
 import './App.css';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path='/' component={Activity} />
-        <Route path='/:sportId' component={Details} />
+        <Switch>
+          <Route exact path='/' component={Activity} />
+          <Route exact path='/not-found' component={NotFound} />
+          <Route exact path='/:sportId' component={Details} />
+        </Switch>
       </div >
     </Router>
   );
