0b55f477b21146c331cc42fc2be1de5fb50d245c
mar1n <30738216+mar1n@users.noreply.github.com>
2019-11-26 11:52:06 +0000

    change port

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 const activityList = require('./activity-get')
-
+const port = process.env.PORT || 3001
 app.get('/rest/shows', (req, res) => {
    return res.send(activityList);
  });
 
-var server = app.listen(3001, function () {
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
