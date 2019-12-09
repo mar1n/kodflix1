f85fd36143a17f1abb377443d5c34ba8a311a98a
mar1n <30738216+mar1n@users.noreply.github.com>
2019-12-09 22:34:16 +0000

    dadada

--- src/backend/db.js
+++ src/backend/db.js
@@ -11,7 +11,7 @@ module.exports = { connect };
 
 function connect() {
     return new Promise((resolve) => {
-        MongoClient.connect(url2, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, mongoDB) {
+        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, mongoDB) {
             if (err) throw err;
             const dbo = mongoDB.db(dbName);
             resolve(dbo); 
