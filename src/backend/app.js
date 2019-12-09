f8006f1998bd63c20034194f4739f915594f4855
mar1n <30738216+mar1n@users.noreply.github.com>
2019-12-09 15:32:40 +0000

    connect with cloud

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,21 +1,42 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const activityList = require('./activity-get')
+//const activityList = require('./activity-get')
 const port = process.env.PORT || 3001;
+const db = require('./db')
 
-app.get('/rest/activity', (req, res) => {
-   return res.send(activityList);
+
+
+// db.connect
+// app.get('/rest/activity', (req, res) => {
+//    return res.send(activityList);
+// });
+
+// app.use(express.static(path.join(__dirname, '../../build')));
+
+// app.get('*', function (req, res) {
+//    res.sendFile(path.join(__dirname, '../../build', './public/index.html'));
+// });
+
+// var server = app.listen(port, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+db.connect().then(dbo => {
+   app.get('/rest/activity', (req, res) => {
+      dbo.collection('sport').find({}).toArray((err, results) => {
+          if (err) throw err;
+          res.send(results);
+      });
+  });
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+  });
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 });
-
-app.use(express.static(path.join(__dirname, '../../build')));
-
-app.get('*', function (req, res) {
-   res.sendFile(path.join(__dirname, '../../build', './public/index.html'));
-});
-
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
---
+++ src/backend/db.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const url = "mongodb://localhost:27017";
+const url2 = process.env.NODE_ENV = 'production' ?
+    process.env.DB_URL_PRD :
+    process.env.DB_URL_DEV;
+const dbName2 = url2.substr(url2.lastIndexOf('/') + 1);
+const dbName = 'sport'
+
+module.exports = { connect };
+
+function connect() {
+    return new Promise((resolve) => {
+        MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, mongoDB) {
+            if (err) throw err;
+            const dbo = mongoDB.db(dbName2);
+            resolve(dbo); 
+          });
+    });
+}
