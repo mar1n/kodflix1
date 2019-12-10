3f237ca89868b92db72d3d18b27f8f24d3a15cff
mar1n <30738216+mar1n@users.noreply.github.com>
2019-12-10 15:44:41 +0000

    add google analytics

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
 import Activity from './Activity.js';
 import Details from './Details/Details.js';
 import './App.css';
 import NotFound from './NotFound';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path='/' component={Activity} />
-          <Route exact path='/not-found' component={NotFound} />
-          <Route exact path='/:sportId' component={Details} />
-        </Switch>
-      </div >
-    </Router>
-  );
+class App extends React.Component {
+  constructor({ history, location}) {
+    super();
+    ReactGA.initialize('UA-154355013-1');
+    this.trackPageView(location);
+    history.listen((location) => this.trackPageView(location));
+  }
+
+  trackPageView(location) {
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+  }
+  render() {
+    return (
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Activity} />
+            <Route exact path='/not-found' component={NotFound} />
+            <Route exact path='/:sportId' component={Details} />
+          </Switch>
+        </div >
+    );
+  }
+  
 }
 
-export default App;
+export default withRouter(App);
